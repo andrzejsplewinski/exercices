570487c7a40c0aa3aadbd36671be0d8225f0b282
Rzesiasty <andrzej.splewinski@gmail.com>
2023-04-05 17:48:21 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,14 @@ import java.lang.*;
 import java.io.*;
 public class Main {
     public static void main (String[] args) throws java.lang.Exception {
-        Punkt1 punkt1 = new Punkt1();
-        punkt1.age = 16;
-        punkt1.sex = 'k';
+        System.out.println("Starting...");
+        int age = 15;
+        char sex = 'm';
 
-        System.out.println("Starting");
-            if (punkt1.age <= 15) {
-                System.out.println("Too young");
-            } else {
-                System.out.println("It's okay!");
-                if (punkt1.sex == 'm') {
-                    System.out.println("User is a male");
-                } else if (punkt1.sex == 'f') {
-                    System.out.println("User is a female");
-                }
-                else {
-                    System.out.println("User sex is unknown");
-                }
-            }
+        userValidator validator = new userValidator();
+        validator.validateAge(age);
+        validator.validateSex(sex);
+
         System.out.println("End of the program");
     }
 }
--- src/userValidator.java
+++ src/userValidator.java
@@ -1,8 +1,21 @@
-public class Punkt{
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class userValidator {
     public void validateAge(int age) {
-
+        if (age <= 15){
+            System.out.println("Too young!");
+        } else {
+            System.out.println("User age is ok");
+        }
     }
     public void validateSex(char sex){
-        
+        if (sex == 'm'){
+            System.out.println("User is male");
+        } else if (sex == 'f'){
+            System.out.println("User is female");
+        } else {
+            System.out.println("Unknown sex");
+        }
     }
 }
