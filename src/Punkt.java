c7de3853be0a675d0b0f8b0aec179ecc71c20ebc
Rzesiasty <andrzej.splewinski@gmail.com>
2023-04-05 17:47:32 +0200

    commit

--- src/Punkt.java
+++
@@ -1,6 +0,0 @@
-public class Punkt{
-    int wspX;
-    int wspY;
-    int wspZ;
-    int wspC;
-}
---
+++ src/userValidator.java
@@ -0,0 +1,8 @@
+public class Punkt{
+    public void validateAge(int age) {
+
+    }
+    public void validateSex(char sex){
+        
+    }
+}
